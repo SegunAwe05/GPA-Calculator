fa564850071e68a2aa128c3a3021d5e6c2554491
Segun Awe <segunawe05@gmail.com>
2021-12-25 21:07:46 -0500

    first commit

---
+++ GPA Calculator/GPA_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalculatorApp.swift
+//  GPA Calculator
+//
+//  Created by Segun Awe on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
